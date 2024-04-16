ed509d1a273d221c9bcc76f7a01799d16bb00584
danielaloiap <67015001+danielaloiap@users.noreply.github.com>
2024-04-16 11:01:20 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("Example Streamlit App :balloon:")
+st.write(
+    """Choose the fruits you want in your Custom Smoothie!
+    """
+)
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+ingredients_list = st.multiselect(
+    'Choose up to 6 ingredients:',
+    my_dataframe,
+    max_selections = 6
+)
+if ingredients_list:
+    ingredients_string = ', '.join(ingredients_list)  # Corrigido para juntar os ingredientes com vírgula
+
+    my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('{ingredients_string}', '{name_on_order}')"""  # Corrigido para incluir duas colunas
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
