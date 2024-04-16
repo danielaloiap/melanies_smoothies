6d785e51c99566caf9d1a43dabcec77ce8c59b58
danielaloiap <67015001+danielaloiap@users.noreply.github.com>
2024-04-16 15:37:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,6 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-conn = connect(
-account = "DDXLLFY.AT93454"
-user = "danielplastina"
-password = "135Mudar@135Alterar"
-role = "SYSADMIN"
-warehouse = "COMPUTE_WH"
-database = "SMOOTHIES"
-schema = "PUBLIC"
-client_session_keep_alive = true
-)
-cursor = conn.cursor()
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -52,5 +41,3 @@ if ingredients_list:
         # Execute a instrução SQL
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-cursor.close()
-conn.close()
