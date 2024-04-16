5450a3426898b6ab43e2963c40f86efd0368c2d2
danielaloiap <67015001+danielaloiap@users.noreply.github.com>
2024-04-16 12:45:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,6 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
-
-    my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('{ingredients_string}', '{name_on_order}')"""  # Corrigido para incluir duas colunas
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
