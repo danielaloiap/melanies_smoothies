bcb15024819b26512b33e39d33a992051441b391
danielaloiap <67015001+danielaloiap@users.noreply.github.com>
2024-04-16 14:49:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,20 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)  # Corrigido para juntar os ingredientes com vírgula
+    ingredients_string = ' '
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
        # st.write('The search value for ', fruit_chosen, 'is', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+     my_insert_stmt = """INSERT INTO smoothies.public.orders (name_on_order, ingredients) VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        # Construa a instrução SQL de inserção
-        my_insert_stmt = f"INSERT INTO smoothies.public.orders (name_on_order, ingredients) VALUES ('{name_on_order}', '{ingredients_string}')"
-
         # Execute a instrução SQL
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
